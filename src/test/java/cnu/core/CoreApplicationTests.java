21ed4481a577de51970d3aa40f04cdf66186650a
Sunkist18 <dandalf@o.cnu.ac.kr>
2025-04-07 16:11:53 +0900

    Gradle 프로젝트 초기 설정: .gitattributes, build.gradle, gradlew, gradlew.bat, settings.gradle, gradle-wrapper.properties, CoreApplication 및 테스트 클래스 추가

---
+++ src/test/java/cnu/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cnu.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
